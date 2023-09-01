e41d272220e0b3b730461b066b1720b4000bd7d7
Pokachupas <oscaryliam@gmail.com>
2023-08-31 23:04:03 -0600

    Se termino la practica 1

--- 001_Practica.py
+++ 001_Practica.py
@@ -1,9 +1,39 @@
-x = 10
-print(x)
+#Aquí se esta creando un objeto, que es un tipo nuevo de variable que posee características específicas
 
-print("Ha cambiado el programa")
-y = 15
-if x == y:
-    print(x, " = ", y)
-else:
-    print(x," no es igual a ", y)
+class Usuario:
+    name = "xxxx"  #Aquí se utiliza una variable para representar una característica o "atributo" del objeto
+    password = "xxxx"
+    edad = 0000
+    
+    def __init__(self): 
+        return
+
+    def setName(self):
+        self.name = input("Ingrese su nombre completo: ")
+        self.name = self.name.title()
+    
+    def setPassword(self):
+        self.password = input("Ingrese su password: ")
+    
+    def setAge(self):
+        self.edad = input("Ingrese su edad: ")
+
+    def showData(self):
+        testPassword = input("Ingrese su contraseña: ")
+        if testPassword == self.password:
+            print(self.name + "\n" + self.password + "\n" + self.edad)
+        else:
+            print("Contraseña incorrecta")
+
+    def setData(self):
+        self.setName()
+        self.setPassword()
+        self.setAge()
+
+
+
+user1 = Usuario()
+user1.setData()
+user1.showData()
+
+
