66513a35ec24185219bac91e6f6e7406f08f292d
Pokachupas <75194562+Pokachupas@users.noreply.github.com>
2023-09-01 09:02:25 -0600

    Update 002_Practica.py

--- 002_Practica.py
+++ 002_Practica.py
@@ -2,4 +2,18 @@
 ##Esta practica abarca del capitulo 7 hasta el capitulo 9 ##
 
 class Calculadora:
-    
+   operandos = []
+
+   def Suma(self):
+      sumaTotaL = 0
+      x = 1
+      while(x):
+         x = input("Ingrese un numero a sumar o 0 para salir")
+         if x != 0:
+            operandos.append(x)
+      #for number in range operandos:
+      #  sumaTotal += number
+      sumaTotal = sum(operandos)
+      print("La suma es: " + sumaTotal)
+   
+         
